8bdf07c2f271a421a63d81c472bae36a814835e8
Aimal Khan <Aimal.khan@bryanuniversity.edu>
2021-06-03 13:17:59 -0400

    week-6

--- week-6/printer/printer.js
+++ week-6/printer/printer.js
@@ -1,11 +1,15 @@
 var printData = function(data){
-    return function() {
-        var sentence = data.toLowerCase().split(" ");
-        for(var i =0; i< sentence.length; i++){
-        sentence[i] = sentence[i][0].toUpperCase()+sentence[i].slice(1);
-        }
-        return sentence.join(" ");
+    return function (){
+    var sentence = data.toLowerCase().split(" ");
+    for(var i=0; i< sentence.length; i++){
+        sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
     }
-}
+    return sentence.join(" ");
+}}
+//const print = printData("Hi there HOW ARE you")
+//console.log(print())
 
-const result = printData("hello THERe");
+const print = printData
+const output = print("Hi there HOW ARE yOU")
+const result = output()
+console.log(result)
